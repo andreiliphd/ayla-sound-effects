c312c261023a66f32dd51092ec41cce6180cee33
Andrei Li <andreiliphd@gmail.com>
2021-09-25 08:28:26 +0300

    Initial Commit

---
+++ ayla-sound-effects/ayla_sound_effectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ayla_sound_effectsApp.swift
+//  ayla-sound-effects
+//
+//  Created by Ли Андрей on 25.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ayla_sound_effectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
