80242e48ad7b256cf2c6f8f99f8077ada5376e72
Andrei Li <andreiliphd@gmail.com>
2021-09-26 23:16:05 +0300

    Assets added. Two buttons added.

--- ayla-sound-effects/ContentView.swift
+++ ayla-sound-effects/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button(action: {
+                print("Button was tapped")
+            }) {
+                Image("Record")
+            }
+
+            Text("Tap to record!")
+                .padding()
+            Button(action: {
+                print("Button was tapped")
+            }) {
+                Image("Stop")
+                    .resizable()
+                    .frame(width: 64, height: 64, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
 }
 
